bcde1165d5d64ed61e5b576da29de8c9177f70af
mengpi <mengweidong@outlook.com>
2021-06-04 11:37:51 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 import FancyButton from './FancyButton';
 
-export default function App() {
-  const ref = React.createRef();
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <FancyButton ref={ref}> Hello </FancyButton>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  buttonClick(index) {
+    console.log('current:', this.myRef.current);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p
+          onClick={() => {
+            this.buttonClick();
+          }}
+        >
+          Start editing to see some magic happen :)
+        </p>
+        <FancyButton ref={this.myRef}> Hello </FancyButton>
+      </div>
+    );
+  }
 }
